7c0657e6310b7e0ca85de044053437c71efdfebc
Ruan Montelo <ruanmontelo@gmail.com>
2020-06-01 23:48:32 -0300

    First commit for the NLW React Webapp project

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let code: string = "src/App.tsx";
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>{code}</code> and save to reload.
         </p>
         <a
           className="App-link"
